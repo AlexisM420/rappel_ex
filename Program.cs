c5aec8ed3e4fa0fdfae41cea44b84f49849c9480
Alexis Michaud <michaud2alexis@hotmail.com>
2020-01-28 03:06:57 -0500

    Exercice 1 complété

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace rappel_ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Alexis Michaud";
+            Console.WriteLine(name);
+        }
+    }
+}
