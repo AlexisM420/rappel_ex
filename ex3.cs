96260e801b74600129a55bdd4a13ce3a94e57a40
Alexis Michaud <michaud2alexis@hotmail.com>
2020-01-28 03:09:57 -0500

    Exercice 3 complété

---
+++ ex3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rappel_ex01
+{
+    class ex3
+    {
+        var name = "Alexis Michaud";
+        Console.WriteLine(name);
+    }
+}
